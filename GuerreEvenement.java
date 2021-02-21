d3937f91187e1845cb38f59ffe2cb31c2383562d
Gateub <56395732+Gateub@users.noreply.github.com>
2021-02-21 18:31:53 +0100

    Add files via upload

---
+++ GuerreEvenement.java
@@ -0,0 +1,38 @@
+package evenements;
+
+import campagne.Campagne;
+import factions.Faction;
+
+public class GuerreEvenement extends Evenement{
+
+    private String description;
+
+    public GuerreEvenement() {
+        super();
+        this.description = "La Russie veut vous déclarer la guerre.";
+        this.ajouterChoix("1-A la guerre ! Ils vont voir ces buveurs de vodka !  (-60 d'approbation des communistes)");
+        this.ajouterChoix("2-Attendez on peut négocier non ? ");
+
+
+    }
+
+    @Override
+    public void consequences( int choix, Economie economie, Campagne campagne) {
+        if(choix == 1){
+            campagne.getFactions()[1].diminuerApprobation(60);
+        }
+
+    }
+
+    @Override
+    public void consequencesFacile(int choix, Economie e, Campagne campagne) {
+
+    }
+
+    @Override
+    public String getDescription() {
+        String s ="";
+        System.out.println(description);
+        return s;
+    }
+}
---
+++ ManifEvenement.java
@@ -0,0 +1,86 @@
+package evenements.Techno;
+
+import campagne.Campagne;
+import evenements.Economie;
+import evenements.Evenement;
+import factions.Faction;
+
+public class ManifEvenement extends Evenement{
+    private final String description;
+
+    public ManifEvenement() {
+        super();
+        this.description = "La nouvelle technologie a un impact beaucoup trop nuisible sur l'écologie, une manifestation ECOLO est donc organisé par beaucoup d'habitants";
+        this.ajouterChoix("1- Envoyer l'armée et tuer tout manifestant");
+        this.ajouterChoix("2- Laisser la manifestation se dérouler et ne rien changer aux lois");
+        this.ajouterChoix("3- Se plier à leurs convictions");
+    }
+
+    @Override
+    public void consequences( int choix, Economie e, Campagne campagne) {
+        if(choix == 1){
+            campagne.getFactions()[4].diminuerApprobation(30);
+            campagne.getFactions()[3].augmenterApprobation(10);
+            campagne.getFactions()[2].diminuerApprobation(5);
+        }
+        else if (choix == 2){
+            campagne.getFactions()[6].augmenterApprobation(10);
+            campagne.getFactions()[0].augmenterApprobation(5);
+            campagne.getFactions()[4].diminuerApprobation(5);
+        }
+        else if (choix == 3){
+            campagne.getFactions()[6].diminuerApprobation(5);
+            campagne.getFactions()[0].diminuerApprobation(5);
+            campagne.getFactions()[4].augmenterApprobation(25);
+        }
+        
+    }
+
+    @Override
+    public void consequencesFacile(int choix, Economie e, Campagne campagne) {
+    	if(choix == 1){
+            campagne.getFactions()[4].diminuerApprobation(15);
+            campagne.getFactions()[3].augmenterApprobation(20);
+            campagne.getFactions()[2].diminuerApprobation(2);
+        }
+        else if (choix == 2){
+            campagne.getFactions()[6].augmenterApprobation(20);
+            campagne.getFactions()[0].augmenterApprobation(10);
+            campagne.getFactions()[4].diminuerApprobation(2);
+        }
+        else if (choix == 3){
+            campagne.getFactions()[6].diminuerApprobation(2);
+            campagne.getFactions()[0].diminuerApprobation(2);
+            campagne.getFactions()[4].augmenterApprobation(50);
+        }
+
+    }
+    
+    public void consequencesDifficile( int choix, Economie e, Campagne campagne) {
+        if(choix == 1){
+            campagne.getFactions()[4].diminuerApprobation(50);
+            campagne.getFactions()[3].augmenterApprobation(5);
+            campagne.getFactions()[2].diminuerApprobation(10);
+        }
+        else if (choix == 2){
+            campagne.getFactions()[6].augmenterApprobation(5);
+            campagne.getFactions()[0].augmenterApprobation(10);
+            campagne.getFactions()[4].diminuerApprobation(10);
+        }
+        else if (choix == 3){
+            campagne.getFactions()[6].diminuerApprobation(10);
+            campagne.getFactions()[0].diminuerApprobation(10);
+            campagne.getFactions()[4].augmenterApprobation(15);
+        }
+        
+    }
+    
+    @Override
+    public String getDescription() {
+        String s ="";
+        System.out.println(description);
+        return s;
+    }
+
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
